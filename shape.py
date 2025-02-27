14117ef2dd0674b8a1cf35d4a37be679e1e06172
Josh <jsbusiness305@gmail.com>
2025-02-27 16:46:38 -0500

    shape example

---
+++ shape.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([[11,12], [5,6]])
+
+#print shape of the arr example
+# returns number of dimensiions and size of each dimension
+print("arr shape : ", arr.shape)
